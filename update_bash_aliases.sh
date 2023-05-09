ba4568237eb64961c0216a1f1bdca8315c15c8d6
Agunloye Oluwatobiloba <jgetitdonefast@gmail.com>
2023-05-09 16:01:38 +0100

    Update update_bash_aliases.sh

--- update_bash_aliases.sh
+++ update_bash_aliases.sh
@@ -6,3 +6,4 @@ BASH_ALIASES_FILE="$HOME/.bash_aliases"
 # Append the text from the file to the .bash_aliases file
 cat >> "$BASH_ALIASES_FILE" <<< ""
 cat < .bash_aliases >> "$BASH_ALIASES_FILE"
+echo "Content copied to $BASH_ALIASES_FILE ✅"
