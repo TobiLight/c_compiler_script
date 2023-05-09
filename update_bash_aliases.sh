fd3f94e19eb0dab433a92f78d414ae0a12c3255c
Agunloye Oluwatobiloba <jgetitdonefast@gmail.com>
2023-05-09 15:58:05 +0100

    Update update_bash_aliases.sh

--- update_bash_aliases.sh
+++ update_bash_aliases.sh
@@ -3,12 +3,6 @@
 # Define the path to the .bash_aliases file
 BASH_ALIASES_FILE="$HOME/.bash_aliases"
 
-# Check that the text file to append from exists
-if [ ! -f "$1" ]; then
-  echo "Error: file not found"
-  echo "Usage: $0 <text_file>"
-  exit 1
-fi
-
 # Append the text from the file to the .bash_aliases file
-cat "\n$1" >> "$BASH_ALIASES_FILE"
+cat >> "$BASH_ALIASES_FILE" <<< ""
+cat < .bash_aliases >> "$BASH_ALIASES_FILE"
